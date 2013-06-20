8c27af4069e8cd638bacd1c26d801c22c3ece618
Brian T.N. Gunney <gunneyb@llnl.gov>
2013-06-20 15:05:21 -0700

    Bring over TileClustering timers from Perf branch.

--- source/SAMRAI/mesh/TileClustering.C
+++ source/SAMRAI/mesh/TileClustering.C
@@ -117,6 +117,8 @@ TileClustering::findBoxesContainingTags(
       t_find_boxes_containing_tags->barrierAndStart();
    }
 
+   t_cluster_setup->start();
+
    const hier::IntVector &zero_vector = hier::IntVector::getZero(tag_level->getDim());
 
    for (hier::BoxContainer::const_iterator bb_itr = bound_boxes.begin();
@@ -142,6 +144,9 @@ TileClustering::findBoxesContainingTags(
       zero_vector);
    tag_to_new->setTranspose(new_to_tag, true);
 
+   tag_box_level.getBoxes().makeTree(tag_box_level.getGridGeometry().get());
+
+   t_cluster_setup->stop();
 
    t_cluster->start();
 
@@ -226,6 +231,7 @@ TileClustering::findBoxesContainingTags(
          t_coalesce->stop();
 
       }
+
       if ( d_print_steps ) {
          tbox::plog << "TileClustering coalesced " << new_box_level->getLocalNumberOfBoxes()
                     << " new boxes into " << box_vector.size() << "\n";
@@ -326,14 +332,13 @@ TileClustering::findBoxesContainingTags(
    if (d_barrier_and_time) {
       t_global_reductions->barrierAndStart();
    }
+
+   t_cluster_wrapup->start();
+
    new_box_level->getGlobalNumberOfBoxes();
    new_box_level->getGlobalNumberOfCells();
    if (d_barrier_and_time) {
       t_global_reductions->barrierAndStop();
-   }
-   for (hier::BoxContainer::const_iterator bi = bound_boxes.begin();
-        bi != bound_boxes.end(); ++bi) {
-      new_box_level->getGlobalBoundingBox(bi->getBlockId().getBlockValue());
    }
 
    if (d_log_cluster) {
@@ -377,6 +382,8 @@ TileClustering::findBoxesContainingTags(
                  << "\tTileClustering tag_to_new statistics:\n" << tag_to_new->formatStatistics("\t\t")
                  << "\n";
    }
+
+   t_cluster_wrapup->stop();
 
    if (d_barrier_and_time) {
       t_find_boxes_containing_tags->barrierAndStop();
@@ -564,6 +571,10 @@ TileClustering::setTimers()
       getTimer("mesh::TileClustering::findBoxesContainingTags()_coalesce");
    t_coalesce_adjustment = tbox::TimerManager::getManager()->
       getTimer("mesh::TileClustering::findBoxesContainingTags()_coalesce_adjustment");
+   t_cluster_setup = tbox::TimerManager::getManager()->
+      getTimer("mesh::TileClustering::findBoxesContainingTags()_setup");
+   t_cluster_wrapup = tbox::TimerManager::getManager()->
+      getTimer("mesh::TileClustering::findBoxesContainingTags()_wrapup");
    t_global_reductions = tbox::TimerManager::getManager()->
       getTimer("mesh::TileClustering::global_reductions");
 }
