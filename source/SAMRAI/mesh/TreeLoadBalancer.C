019079e3fa91885796b3b90525f979164537530d
Brian T.N. Gunney <gunney1@llnl.gov>
2013-12-04 13:24:46 -0800

    Fix logic bug in computing number of root cycles.

--- source/SAMRAI/mesh/TreeLoadBalancer.C
+++ source/SAMRAI/mesh/TreeLoadBalancer.C
@@ -353,7 +353,7 @@ TreeLoadBalancer::loadBalanceBoxLevel(
     * to support such a request and multi-cycling simultaneously.
     */
    const double fanout_size = max_local_load/d_global_avg_load;
-   const int number_of_cycles = rank_group.containsAllRanks() ? 1 :
+   const int number_of_cycles = !rank_group.containsAllRanks() ? 1 :
       int(ceil( log(fanout_size)/log(d_max_cycle_spread_procs) ));
       if (d_print_steps) {
          tbox::plog << "TreeLoadBalancer::loadBalanceBoxLevel"
