b0e3db2e21207b513727ad5f373c54f491bc5546
David Beckingsale <davidbeckingsale@gmail.com>
2017-02-13 07:13:21 -0800

    Adding tests

--- cmake/CMakeBasics.cmake
+++ cmake/CMakeBasics.cmake
@@ -4,6 +4,8 @@ include(cmake/SetupM4.cmake)
 
 include(cmake/SetupCompilers.cmake)
 
+include(cmake/SAMRAIMacros.cmake)
+
 # This needs to happen last, once all we have defined options and checked for
 # third-party packages.
 include(cmake/CMakeConfigureFile.cmake)
