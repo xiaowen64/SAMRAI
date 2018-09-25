71d17d1556cdfb80035215b2aeb0adb1937dfc92
Noah S. Elliott <elliott22@llnl.gov>
2018-09-25 09:52:41 -0700

    Fix up cmake build after merging master--remove boost from cmake files

--- cmake/thirdparty/SetupSAMRAIThirdParty.cmake
+++ cmake/thirdparty/SetupSAMRAIThirdParty.cmake
@@ -7,16 +7,6 @@ else ()
   set(LACKS_MPI True)
 endif ()
 
-# Boost
-find_package(Boost REQUIRED)
-if (${Boost_FOUND})
-  set(HAVE_BOOST_HEADERS True)
-endif ()
-
-blt_register_library(
-  NAME boost
-  INCLUDES ${Boost_INCLUDE_DIR})
-
 if (ENABLE_HDF5)
   if (NOT ENABLE_MPI)
     message(FATAL_ERROR "HDF5 requires MPI.")
