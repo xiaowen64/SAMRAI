7f379fd80c26adc2b1205943f20423bdb8399c41
David Beckingsale <davidbeckingsale@gmail.com>
2018-10-03 09:07:31 -0700

    Add Travis config and script

---
+++ source/scripts/travis/build_and_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+env
+function or_die () {
+    "$@"
+    local status=$?
+    if [[ $status != 0 ]] ; then
+        echo ERROR $status command: $@
+        exit $status
+    fi
+}
+
+or_die mkdir travis-build
+cd travis-build
+if [[ "$DO_BUILD" == "yes" ]] ; then
+    or_die ../configure 
+    or_die make -j 3 VERBOSE=1
+fi
+
+exit 0
