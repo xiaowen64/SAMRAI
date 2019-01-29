0ef5ff0ef719255b5b3ffabd910e9b4b6c886542
David Beckingsale <davidbeckingsale@gmail.com>
2019-01-29 11:38:42 -0800

    Add ctest line to Travis script

--- source/scripts/travis/build_and_test.sh
+++ source/scripts/travis/build_and_test.sh
@@ -14,6 +14,7 @@ cd travis-build
 if [[ "$DO_BUILD" == "yes" ]] ; then
     cmake -DENABLE_MPI=Off -DCMAKE_CXX_COMPILER=`which $COMPILER` -DCMAKE_Fortran_COMPILER=gfortran ../
     or_die make -j 3 VERBOSE=1
+    ctest -j 6
 fi
 
 exit 0
