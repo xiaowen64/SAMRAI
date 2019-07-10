1ef53e39050810e7e29ff62c0685b1d71d9c162c
Johann Dahm <johann.dahm@ibm.com>
2019-07-10 12:01:39 -0700

    Quote compiler env var

--- source/scripts/travis/build_and_test.sh
+++ source/scripts/travis/build_and_test.sh
@@ -16,7 +16,7 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
         (cd Umpire && git checkout v0.3.5) || exit $?
         mkdir -p $umpire_install
         mkdir -p $umpire_build && cd $_
-        cmake -DCMAKE_INSTALL_PREFIX=$umpire_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER=$COMPILER ../Umpire || exit $?
+        cmake -DCMAKE_INSTALL_PREFIX=$umpire_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER="$COMPILER" ../Umpire || exit $?
         make -j $threads install || exit $?
         cd $base_path
     fi
@@ -30,7 +30,7 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
         (cd RAJA && git checkout v0.8.0) || exit $?
         mkdir -p $raja_install
         mkdir -p $raja_build && cd $_
-        cmake -DCMAKE_INSTALL_PREFIX=$raja_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER=$COMPILER ../RAJA || exit $?
+        cmake -DCMAKE_INSTALL_PREFIX=$raja_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER="$COMPILER" ../RAJA || exit $?
         make -j $threads install || exit $?
         cd $base_path
     fi
@@ -41,7 +41,7 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
     cd $samrai_source && git submodule init && git submodule update || exit $?
     samrai_build=$base_path/SAMRAI-build
     mkdir -p $samrai_build && cd $_ || exit $?
-    cmake -DENABLE_MPI=Off -DENABLE_CUDA=OFF -DENABLE_HDF5=Off -DENABLE_RAJA=ON -DRAJA_DIR=$raja_install/share/raja/cmake -DENABLE_UMPIRE=ON -Dumpire_DIR=$umpire_install/share/umpire/cmake -DCMAKE_CXX_COMPILER=$COMPILER -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_EXAMPLES=ON -DENABLE_TESTS=ON -DCMAKE_Fortran_COMPILER=gfortran $samrai_source || exit $?
+    cmake -DENABLE_MPI=Off -DENABLE_CUDA=OFF -DENABLE_HDF5=Off -DENABLE_RAJA=ON -DRAJA_DIR=$raja_install/share/raja/cmake -DENABLE_UMPIRE=ON -Dumpire_DIR=$umpire_install/share/umpire/cmake -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_EXAMPLES=ON -DENABLE_TESTS=ON -DCMAKE_Fortran_COMPILER=gfortran $samrai_source || exit $?
     make -j $threads || exit $?
     [[ "$DO_TEST" == "yes" ]] && ctest --extra-verbose -j $threads || exit $?
 fi
