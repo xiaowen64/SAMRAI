8d49f99fed3f254962ad2058d8f28e444ae77441
David Beckingsale <davidbeckingsale@gmail.com>
2020-04-02 07:27:45 -0700

    Update host-config to add MPI

--- host-configs/toss_3_x86_64_ib/gcc_7_1_0.cmake
+++ host-configs/toss_3_x86_64_ib/gcc_7_1_0.cmake
@@ -1,2 +1,6 @@
-set(DENABLE_MPI Off CACHE BOOL "")
-set(DENABLE_HDF5 Off CACHE BOOL "")
+set(ENABLE_MPI On CACHE BOOL "")
+set(MPI_C_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.1.0/bin/mpicc" CACHE PATH "")
+set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.1.0/bin/mpic++" CACHE PATH "")
+set(MPI_Fortran_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-7.1.0/bin/mpif90" CACHE PATH "")
+
+set(ENABLE_HDF5 Off CACHE BOOL "")
