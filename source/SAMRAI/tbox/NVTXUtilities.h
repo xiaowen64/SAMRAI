1a80246a9590b83f9564905d3298cadee9c802fc
Noah S. Elliott <elliott22@llnl.gov>
2020-04-07 14:53:22 -0700

    Add namespaces in NVTXUtilities.h

--- source/SAMRAI/tbox/NVTXUtilities.h
+++ source/SAMRAI/tbox/NVTXUtilities.h
@@ -17,17 +17,25 @@
 #include "nvToolsExt.h"
 #include "cuda_profiler_api.h"
 
+namespace SAMRAI {
+namespace tbox {
+namespace NVTXUtilities {
+
 const uint32_t colors[] = { 0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff, 0x0000ffff, 0x00ff0000, 0x00ffffff };
 const int num_colors = sizeof(colors)/sizeof(uint32_t);
 
+}
+}
+}
+
 #define RANGE_PUSH(name,cid) { \
    int color_id = cid; \
-   color_id = color_id%num_colors;\
+   color_id = color_id%SAMRAI::tbox::NVTXUtilities::num_colors;\
    nvtxEventAttributes_t eventAttrib = {0}; \
    eventAttrib.version = NVTX_VERSION; \
    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
    eventAttrib.colorType = NVTX_COLOR_ARGB; \
-   eventAttrib.color = colors[color_id]; \
+   eventAttrib.color = SAMRAI::tbox::NVTXUtilities::colors[color_id]; \
    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
    eventAttrib.message.ascii = name; \
    nvtxRangePushEx(&eventAttrib); \
@@ -39,12 +47,12 @@ const int num_colors = sizeof(colors)/sizeof(uint32_t);
 
 #define RANGE_PUSH_SYNC(name,cid) { \
    int color_id = cid; \
-   color_id = color_id%num_colors;\
+   color_id = color_id%SAMRAI::tbox::NVTXUtilities::num_colors;\
    nvtxEventAttributes_t eventAttrib = {0}; \
    eventAttrib.version = NVTX_VERSION; \
    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
    eventAttrib.colorType = NVTX_COLOR_ARGB; \
-   eventAttrib.color = colors[color_id]; \
+   eventAttrib.color = SAMRAI::tbox::NVTXUtilities::colors[color_id]; \
    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
    eventAttrib.message.ascii = name; \
    cudaDeviceSynchronize();\
