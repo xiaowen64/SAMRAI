280fc6b68608e5006578bba28debcf9224262128
Noah S. Elliott <elliott22@llnl.gov>
2020-04-15 16:54:24 -0700

    Add specific path for Fortran in travis SAMRAI build

--- source/scripts/travis/build_and_test_umpire.sh
+++ source/scripts/travis/build_and_test_umpire.sh
@@ -30,7 +30,7 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
         (cd RAJA && git checkout v0.11.0) || exit $?
         mkdir -p $raja_install
         mkdir -p $raja_build && cd $_
-        cmake -DCMAKE_INSTALL_PREFIX=$raja_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran ../RAJA || exit $?
+        cmake -DCMAKE_INSTALL_PREFIX=$raja_install -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_CUDA=OFF -DENABLE_OPENMP=OFF -DENABLE_MPI=OFF -DENABLE_EXAMPLES=OFF -DENABLE_BENCHMARKS=OFF -DENABLE_TESTS=OFF -DCMAKE_CXX_COMPILER="$COMPILER" ../RAJA || exit $?
         make -j $threads install || exit $?
         cd $base_path
     fi
@@ -42,7 +42,7 @@ if [[ "$DO_BUILD" == "yes" ]] ; then
     cd $samrai_source && git submodule init && git submodule update || exit $?
     samrai_build=$base_path/SAMRAI-build
     mkdir -p $samrai_build && cd $_ || exit $?
-    cmake -DENABLE_MPI=Off -DENABLE_CUDA=OFF -DENABLE_HDF5=Off -DENABLE_RAJA=ON -DRAJA_DIR=$raja_install/share/raja/cmake -DENABLE_UMPIRE=ON -Dumpire_DIR=$umpire_install/share/umpire/cmake -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_EXAMPLES=ON -DENABLE_TESTS=ON -DCMAKE_Fortran_COMPILER=gfortran $samrai_source || exit $?
+    cmake -DENABLE_MPI=Off -DENABLE_CUDA=OFF -DENABLE_HDF5=Off -DENABLE_RAJA=ON -DRAJA_DIR=$raja_install/share/raja/cmake -DENABLE_UMPIRE=ON -Dumpire_DIR=$umpire_install/share/umpire/cmake -DCMAKE_CXX_COMPILER="$COMPILER" -DCMAKE_CXX_FLAGS=$CMAKE_EXTRA_FLAGS -DENABLE_EXAMPLES=ON -DENABLE_TESTS=ON -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran $samrai_source || exit $?
     make -j $threads || exit $?
     [[ "$DO_TEST" == "yes" ]] && ctest --extra-verbose -j $threads || exit $?
 fi
