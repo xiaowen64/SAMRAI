67ee2470b72c80bcf791db108990037587b8b785
Noah S. Elliott <elliott22@llnl.gov>
2020-04-23 15:40:20 -0700

    Make serial tests use parallel launch commands when built with MPI

--- cmake/SAMRAIMacros.cmake
+++ cmake/SAMRAIMacros.cmake
@@ -16,10 +16,17 @@ macro (samrai_add_tests)
     get_filename_component(short_test_file ${test_file} NAME)
     set(test_name "${base_name}_test_${short_test_file}")
 
-    blt_add_test(NAME ${test_name}
-      COMMAND ${arg_EXECUTABLE} ${command_args}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-
+    if(ENABLE_MPI)
+      blt_add_test(NAME ${test_name}
+        COMMAND ${arg_EXECUTABLE} ${command_args}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        NUM_MPI_TASKS 1)
+    else()
+      blt_add_test(NAME ${test_name}
+        COMMAND ${arg_EXECUTABLE} ${command_args}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    endif()  
+ 
     if(${arg_PARALLEL})
       set(test_name "${base_name}_test_${short_test_file}_2")
       blt_add_test(NAME ${test_name}
